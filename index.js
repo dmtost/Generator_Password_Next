1af5d2c11deaccfe25894477783c74a81db90c04
DmOST <fantur.dmitry@yandex.ru>
2024-11-27 00:00:29 +0300

    Дефолтный шаблон

---
+++ index.js
@@ -0,0 +1,9 @@
+const lengthPass = document.querySelector("#length-pass");
+const resultGeneratePass = document.querySelector(".result");
+const btnGenerate = document.querySelector(".config__btn-gen");
+const btnCopy = document.querySelector(".config__btn-copy");
+
+// resultGeneratePass.textContent = lengthPass.value;
+// lengthPass.addEventListener("change", (e) => {
+//   resultGeneratePass.textContent = lengthPass.value;
+// });
