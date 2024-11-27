27a048ec32620faabd2765db83ee6ad33d5805de
DmOST <fantur.dmitry@yandex.ru>
2024-11-27 14:16:23 +0300

    feat: доработана форма

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+//n элементы формы
 const lengthPass = document.querySelector("#length-pass");
+const allChars = document.querySelector("#allChars");
+const onlyAplh = document.querySelector("#onlyAplh");
+const onSymbols = document.querySelector("#onSymbols");
+
 const resultGeneratePass = document.querySelector(".result");
 const btnGenerate = document.querySelector(".config__btn-gen");
 const btnCopy = document.querySelector(".config__btn-copy");
 
-// resultGeneratePass.textContent = lengthPass.value;
-// lengthPass.addEventListener("change", (e) => {
-//   resultGeneratePass.textContent = lengthPass.value;
-// });
+resultGeneratePass.textContent = lengthPass.value;
+lengthPass.addEventListener("change", (e) => {
+  resultGeneratePass.textContent = lengthPass.value;
+});
