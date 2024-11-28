13e6c3b49a7312e1337817a87e6fb9452425c2c8
DmOST <fantur.dmitry@yandex.ru>
2024-11-28 17:44:22 +0300

    feat: генерация пароля по выбору

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 //n элементы формы
-const lengthPass = document.querySelector("#length-pass");
-const allChars = document.querySelector("#allChars");
-const onlyAplh = document.querySelector("#onlyAplh");
-const onSymbols = document.querySelector("#onSymbols");
+const form = document.forms.mainForm;
+const lengthPass = form.elements.passLen;
+const btnGeneratePass = form.elements["create-pass"];
+const rButton = form.elements.selection;
 
 const resultGeneratePass = document.querySelector(".result");
-const btnGenerate = document.querySelector(".config__btn-gen");
 const btnCopy = document.querySelector(".config__btn-copy");
 
-resultGeneratePass.textContent = lengthPass.value;
-lengthPass.addEventListener("change", (e) => {
-  resultGeneratePass.textContent = lengthPass.value;
+function checkBoxCheked() {
+  const passwordType = document.querySelector(
+    'input[name="selection"]:checked'
+  ).value;
+
+  const charsUpper = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  const charsLower = "abcdefghijklmnopqrstuvwxyz";
+  const charsNumber = "0123456789";
+  const charsSymbol = "!@#$%^&*()_:>?<";
+
+  const jointCharsAll = `${charsUpper}${charsLower}${charsNumber}${charsSymbol}`;
+  const jointCharsAlphSymb = `${charsUpper}${charsLower}${charsSymbol}`;
+  const jointNumSymb = `${charsNumber}${charsSymbol}`;
+
+  let charset = "";
+  switch (passwordType) {
+    case "all":
+      charset = jointCharsAll;
+      break;
+    case "lettersymbols":
+      charset = jointCharsAlphSymb;
+      break;
+    case "numssymbols":
+      charset = jointNumSymb;
+      break;
+  }
+
+  return charset;
+}
+
+function generatePass(lengthPass, chars) {
+  let resultPass = "";
+
+  for (let i = 0; i < lengthPass; i++) {
+    const genRandomPass = Math.floor(Math.random() * chars.length);
+    resultPass += chars[genRandomPass];
+  }
+  return resultPass;
+}
+
+form.addEventListener("submit", (e) => {
+  resultGeneratePass.innerText = generatePass(
+    lengthPass.value,
+    checkBoxCheked()
+  );
+  e.preventDefault();
 });
